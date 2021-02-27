66792dabdf1593702fab4aadef1e8eff8aa4ef09
mayerror <mayerror@gmail.com>
2021-02-27 17:07:02 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array !== undefined && array.length > 0) {
+    let result = array.reduce((acc, currentNum) => {
+      if (currentNum < acc) {
+        acc = currentNum;
+      }
+      return acc;
+    });
+  
+    return result;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array !== undefined && array.length > 0) {
+    let result = array.reduce((acc, currentNum) => {
+      if (currentNum > acc) {
+        acc = currentNum;
+      }
+      return acc;
+    });
+  
+    return result;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array !== undefined && array.length > 0) {
+    let result = array.reduce((acc, currentNum) => {
+      return acc += currentNum;
+    });
+    return result / array.length;
+  } else {
+    return 0;
+  }
 }
